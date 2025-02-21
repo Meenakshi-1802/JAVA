7bd397fb6052f1bf6b6d6c173aa1a2a05ddd41fa
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2025-02-21 22:22:46 +0530

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,12 @@
+//comments
+public class Comments 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World");//This is a comment
+        System.out.println("Good Evening");/*This code will print the words Good Evening to the screen,and it is amazing */
+
+        
+    }
+    
+}
