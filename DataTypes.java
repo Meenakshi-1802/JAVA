8791d9db27a0658fecd971a47de3b32d04e686ee
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2025-02-21 22:22:14 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,19 @@
+//Data Types
+public class DataTypes 
+{
+    public static void main(String[]args) 
+    {
+        int myNum = 20;
+        float myFloatNum = 55.11f;
+        char myLetter = 'M';
+        boolean myBool = true;
+        String myText = "Hello";
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+
+    }
+    
+}
