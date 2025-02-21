ff472edc2cf73b03fc6b560aa901c36b97fd8c8c
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2025-02-21 22:23:10 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,14 @@
+//Add variables
+public class Add 
+{
+    public static void main(String[] args) 
+    {
+        String fName = "John";
+        String lName = "Doe";
+        String fullName = fName + lName;
+        System.out.println(fullName);
+
+
+    }
+    
+}
