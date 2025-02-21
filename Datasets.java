07407cc5224775f5bd172ff965dbe007dc0398e1
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2025-02-21 22:23:34 +0530

    Add files via upload

---
+++ Datasets.java
@@ -0,0 +1,19 @@
+//Student data
+public class Datasets 
+{
+    public static void main(String[] args) 
+    {
+        String studentName = "John Doe";
+        int studentId = 15;
+        int studentAge = 23;
+        float studentFee = 75.22f;
+        char studentGrade = 'B';
+
+        System.out.println("Student name:" + studentName);
+        System.out.println("Student id:" + studentId);
+        System.out.println("Student age:" + studentAge);
+        System.out.println("Student fee:" + studentFee);
+        System.out.println("Student grade:" + studentGrade);
+          }
+    
+}
