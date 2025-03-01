d27bd937faeac562d4536daac4cd62eb008f2e66
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2025-03-01 10:39:16 +0530

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class IfElse
+{
+public static void main(String args[])
+{
+int n = 10;
+if (n > 5)
+{
+System.out.println("The number is grater than 5.");
+}
+else 
+{
+System.out.println("The number is 5 or less.");
+}
+}
+}
