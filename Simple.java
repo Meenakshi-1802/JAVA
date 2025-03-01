8a5948029b79123298ebd5c69e3477f7fb02b3d0
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2025-03-01 09:04:44 +0530

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Simple
+{
+public static void main(String args[])
+{
+int age;
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter your Age:");
+age = sc.nextInt();
+if(age>=18)
+{
+System.out.println("Eligible for Drive..");
+}
+System.out.println("Thank You!");
+}
+}
+
