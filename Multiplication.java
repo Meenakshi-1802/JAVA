b2852ae477140e73e9c39c205e89482bfca622ff
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2025-03-01 09:00:25 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Multiplication
+{
+public static void main(String args[])
+{
+int x,y,z;
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter value of x & y");
+x = sc.nextInt();
+y = sc.nextInt();
+z = x * y;
+System.out.println("mul="+z);
+}
+}
