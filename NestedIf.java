e5dddb15852c4b998ad665a8368ce6ba229c428f
MEENAKSHI PALAI <meenakshipalai368@gmail.com>
2025-03-01 10:39:38 +0530

    Add files via upload

---
+++ NestedIf.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class NestedIf
+{
+public static void main(String args[])
+{
+int temp = 25;
+if(temp > 25)
+{
+System.out.println("Hot..");
+}
+else
+{
+if(temp > 35)
+{
+System.out.println("Too Hot..");
+}
+else
+{
+System.out.println(" Its Cool..");
+}
+}
+System.out.println("Progran ended!!");
+}
+}
